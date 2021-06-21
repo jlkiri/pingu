b40bdd61759936b6161188d95460b2e7e0214fbf
Kirill Vasiltsov <interalia.dev@gmail.com>
2021-06-22 02:13:50 +0900

    raw loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-use pnet::datalink;
+use socket2::{Domain, Socket, Type};
+use std::net::{SocketAddr, TcpListener};
+use std::io::Result;
 
-fn main() {
-    for interface in datalink::interfaces() {
-        println!("{}", interface);
-    }
+fn main() -> Result<()> {
+    // Create a TCP listener bound to two addresses.
+    let socket = Socket::new(Domain::PACKET, Type::DGRAM, None)?;
+
+    let address: SocketAddr = "[::1]:12345".parse().unwrap();
+    socket.bind(&address.into())?;
+    socket.set_only_v6(false)?;
+    socket.listen(128)?;
+
+    let listener: TcpListener = socket.into();
+
+    Ok(())
 }
